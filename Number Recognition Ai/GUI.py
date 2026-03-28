934e435b206cfe4e0cf2ee19b0c2330a734aeb7f
LT8wrage <151398878+LT8wrage@users.noreply.github.com>
2026-03-28 20:14:55 +0100

    Add GUI for digit recognition using Tkinter

---
+++ Number Recognition Ai/GUI.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from PIL import Image, ImageDraw
+import numpy as np
+
+pil_image = Image.new("RGB", (400, 400), "BLACK")
+pil_draw = ImageDraw.Draw(pil_image)
+
+gui = tk.Tk()
+gui.title("Digit Recognition")
+gui.geometry("500x600")
+
+def draw(event):
+    x, y = event.x, event.y
+    canvas.create_oval(x - 8, y - 8, x + 8, y + 8, fill="white", outline="white")
+    pil_draw.ellipse([x - 8, y - 8, x + 8, y + 8], fill="white", outline="white")
+
+def clear():
+    canvas.delete("all")
+
+def submit():
+    pil_image.save("canvas.png")
+
+title = tk.Label(gui, text='Draw a number')
+canvas = tk.Canvas(gui, width=400, height=400, bg="black")
+Submit_btn = tk.Button(gui, text="Submit", width=10, height=2, command=submit)
+Clear_btn = tk.Button(gui, text="Clear", width=10, height=2, command=clear)
+
+
+
+canvas.bind("<B1-Motion>", draw)
+
+title.pack(pady=30)
+canvas.pack(pady=10)
+Submit_btn.pack(ipadx=100)
+Clear_btn.pack()
+gui.mainloop()
