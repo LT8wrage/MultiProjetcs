c28c8b20460e67e7c6f554cc31bc000a330d70d7
LT8wrage <151398878+LT8wrage@users.noreply.github.com>
2026-06-06 00:34:04 +0100

    Add comments to 3D objects rotation script

--- 3D_OBJECTS/3dobjectsrotation.py
+++ 3D_OBJECTS/3dobjectsrotation.py
@@ -2,26 +2,26 @@ import tkinter as tk
 import math
 
 root = tk.Tk()
-root.title("3D OBJECTS")
+root.title("3D OBJECTS") #creates gui
 
 canvas = tk.Canvas(root, width=600, height=400)
-canvas.pack()
+canvas.pack() #creates canvas
 points = {}
 
 for x in (-1, 1):
     for y in (-1, 1):
         for z in (-1, 1):
-            points[(x,y,z)] = canvas.create_oval(295,195,305,205)
+            points[(x,y,z)] = canvas.create_oval(295,195,305,205) #creates points 
 
 def update(_=None):
     pitch = math.radians(sliderPITCH.get())
     yaw = math.radians(sliderYAW.get())
-    roll = math.radians(sliderROLL.get())
+    roll = math.radians(sliderROLL.get()) #takes value from sliders
 
     for (x, y, z), point_id in points.items():
 
-        x_2d = 300 + ((x*math.cos(yaw)+z*math.sin(yaw))*math.cos(roll)-(y*math.cos(pitch)+x*math.sin(yaw)*math.sin(pitch)-z*math.cos(yaw)*math.sin(pitch))*math.sin(roll))*100
-        y_2d = 200 + ((x*math.cos(yaw)+z*math.sin(yaw))*math.sin(roll)+(y*math.cos(pitch)+x*math.sin(yaw)*math.sin(pitch)-z*math.cos(yaw)*math.sin(pitch))*math.cos(roll))*100
+        x_2d = 300 + ((x*math.cos(yaw)+z*math.sin(yaw))*math.cos(roll)-(y*math.cos(pitch)+x*math.sin(yaw)*math.sin(pitch)-z*math.cos(yaw)*math.sin(pitch))*math.sin(roll))*100 # calculates x values of the points
+        y_2d = 200 + ((x*math.cos(yaw)+z*math.sin(yaw))*math.sin(roll)+(y*math.cos(pitch)+x*math.sin(yaw)*math.sin(pitch)-z*math.cos(yaw)*math.sin(pitch))*math.cos(roll))*100 # calculates y values of the points
 
         canvas.coords(
             point_id,
@@ -29,14 +29,14 @@ def update(_=None):
             y_2d - 4,
             x_2d + 4,
             y_2d + 4
-        )
+        ) # adjusts the coordinates to the canvas
 
 sliderPITCH = tk.Scale(root,from_=0,to=360,orient="horizontal",command=update)
 sliderYAW = tk.Scale(root,from_=0,to=360,orient="horizontal",command=update)
-sliderROLL = tk.Scale(root,from_=0,to=360,orient="horizontal",command=update)
+sliderROLL = tk.Scale(root,from_=0,to=360,orient="horizontal",command=update) # creates the sliders
 
 sliderPITCH.pack()
 sliderYAW.pack()
 sliderROLL.pack()
 
-root.mainloop()
+root.mainloop() #runs the gui or idk
