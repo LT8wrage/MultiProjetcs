1fb52a8f77477f4aceb3833cfb32bc718cca45c4
LT8wrage <151398878+LT8wrage@users.noreply.github.com>
2026-06-06 05:51:36 +0100

    Preparating to add new shapes

--- 3D_OBJECTS/3dobjectsrotation.py
+++ 3D_OBJECTS/3dobjectsrotation.py
@@ -6,17 +6,62 @@ root.title("3D OBJECTS") #creates gui
 
 canvas = tk.Canvas(root, width=600, height=400)
 canvas.pack() #creates canvas
+phi = (1 + math.sqrt(5))/2
+inv = 1/phi
 points = {}
 
-for x in (-1, 1):
-    for y in (-1, 1):
-        for z in (-1, 1):
-            points[(x,y,z)] = canvas.create_oval(295,195,305,205) #creates points 
+
+
+objects = {
+    "cube": {
+        "points": [(1, 1, 1),(1, 1, -1),(1, -1, 1),(1, -1, -1),(-1, 1, 1),(-1, 1, -1),(-1, -1, 1),(-1, -1, -1),],
+        "edges": [((-1,-1,-1),(1,-1,-1)),((-1,-1,-1),(-1,1,-1)),((-1,-1,-1),(-1,-1,1)),
+                  ((1,1,1),(-1,1,1)),((1,1,1),(1,-1,1)),((1,1,1),(1,1,-1)),
+                  ((-1,1,1),(-1,-1,1)),((-1,1,1),(-1,1,-1)),((1,-1,-1),(1,-1,1)),
+                  ((1,-1,1),(-1,-1,1)),((1,1,-1),(1,-1,-1)),((1,1,-1),(-1,1,-1)),]
+    },
+    "octaheron": {"points": [(1, 0, 0),(-1, 0, 0),(0, 1, 0),(0, -1, 0),(0, 0, 1),(0, 0, -1),],
+        "edges": []
+    },
+    "dodecahedron": {"points": [(0, inv, phi),(0, -inv, phi),(0, inv, -phi),(0, -inv, -phi),
+                          (inv, phi, 0),(-inv, phi, 0),(inv, -phi, 0),(-inv, -phi, 0),
+                          (phi, 0, inv),(-phi, 0, inv),(phi, 0, -inv),(-phi, 0, -inv)],
+        "edges": []}
+}
+
+
+for p in objects:
+    points[p] = canvas.create_oval(0,0,0,0)
+
+edges = [
+    ((-1,-1,-1),(1,-1,-1)),
+    ((-1,-1,-1),(-1,1,-1)),
+    ((-1,-1,-1),(-1,-1,1)),
+
+    ((1,1,1),(-1,1,1)),
+    ((1,1,1),(1,-1,1)),
+    ((1,1,1),(1,1,-1)),
+
+    ((-1,1,1),(-1,-1,1)),
+    ((-1,1,1),(-1,1,-1)),
+
+    ((1,-1,-1),(1,-1,1)),
+    ((1,-1,1),(-1,-1,1)),
+
+    ((1,1,-1),(1,-1,-1)),
+    ((1,1,-1),(-1,1,-1)),
+]
+lines = []
+
+for _ in edges:
+    lines.append(canvas.create_line(0,0,0,0))
 
 def update(_=None):
     pitch = math.radians(sliderPITCH.get())
     yaw = math.radians(sliderYAW.get())
-    roll = math.radians(sliderROLL.get()) #takes value from sliders
+    roll = math.radians(sliderROLL.get())#takes value from sliders
+
+    transformed = {}
 
     for (x, y, z), point_id in points.items():
 
@@ -29,7 +74,17 @@ def update(_=None):
             y_2d - 4,
             x_2d + 4,
             y_2d + 4
-        ) # adjusts the coordinates to the canvas
+        )   # adjusts the coordinates to the canvas
+        transformed[(x,y,z)] = (x_2d, y_2d)
+
+    for i, (a, b) in enumerate(edges):
+        x1, y1 = transformed[a]
+        x2, y2 = transformed[b]
+
+        canvas.coords(lines[i], x1, y1, x2, y2)
+    
+
+
 
 sliderPITCH = tk.Scale(root,from_=0,to=360,orient="horizontal",command=update)
 sliderYAW = tk.Scale(root,from_=0,to=360,orient="horizontal",command=update)
@@ -38,5 +93,7 @@ sliderROLL = tk.Scale(root,from_=0,to=360,orient="horizontal",command=update) # 
 sliderPITCH.pack()
 sliderYAW.pack()
 sliderROLL.pack()
+
+root.mainloop() #runs the gui or idk
 
 root.mainloop() #runs the gui or idk
