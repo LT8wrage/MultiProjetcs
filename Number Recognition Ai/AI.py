ab6667251d80b2eeff3521e6c4ebc8d7c010df06
LT8wrage <151398878+LT8wrage@users.noreply.github.com>
2026-06-10 21:01:34 +0100

    Refactor MNIST data loading and normalization
    Removed idx2numpy usage for loading MNIST data and replaced it with keras.datasets.mnist. Adjusted the code for normalizing image data and evaluating the model.

--- Number Recognition Ai/AI.py
+++ Number Recognition Ai/AI.py
@@ -1,39 +1,19 @@
-import os #to set up the base path of the file
-import idx2numpy # convert idx files into numpy arrays (images and labels) 
-import numpy as np
+import os #to set up the base path of the file 
 import keras
 from keras import models, layers
-import tensorflow as tf
+from tensorflow.keras.datasets import mnist
 
-
-base_path = os.path.dirname(__file__) #setting the base path to the file's path to avoid further problems regarding the path
-
-x_train = idx2numpy.convert_from_file(
-    os.path.join(base_path, 'train-images.idx3-ubyte')
-)
-
-y_train = idx2numpy.convert_from_file(
-    os.path.join(base_path, 'train-labels.idx1-ubyte')
-)
-
-x_test = idx2numpy.convert_from_file(
-    os.path.join(base_path, 't10k-images.idx3-ubyte')
-)
-
-y_test = idx2numpy.convert_from_file(
-    os.path.join(base_path, 't10k-labels.idx1-ubyte')
-)
-#converting data bases into code readable images and propts
-
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = x_train / 255.0
 x_test = x_test / 255.0
-#normalising images so it can give us a value between 0 and 1 for easier manipulation while working with neurons
+base_path = os.path.dirname(__file__) #setting the base path to the file's path to avoid further problems regarding the path
 
 model = models.Sequential([
     layers.Flatten(input_shape=(28, 28)),
+    layers.Dense(256, activation='relu'),
     layers.Dense(128, activation='relu'),
-    layers.Dense(10, activation='softmax')
+    layers.Dense(10, activation='softmax') #comnverts values into probabilities
 ])
 
 model.compile(
@@ -42,7 +22,6 @@ model.compile(
     metrics=['accuracy']
 )
 model.fit(x_train, y_train, epochs=5)
-model.evaluate(x_test, y_test)
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print(f"Test Accuracy: {test_acc*100:.2f}%")
 
